69f9b81bb3ba9a8e0f6860401d65e1079d9c1c87
ravi333prakash <76762212+ravi333prakash@users.noreply.github.com>
2020-12-30 09:11:01 +0530

    removed 2nd extra line

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 {
 System.out.println("My first practise on GITHub");
 System.out.println("Adding 1st extra line");
-System.out.println("Adding 2nd extra line");
+
 
 }
 }
